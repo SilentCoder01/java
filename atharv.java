d32f8f8768f05d02a5adc2f270b32ad2618a5b63
SilentCoder01 <ahireatharv64@gmail.com>
2025-01-30 10:59:28 +0530

    Add files via upload

---
+++ atharv.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class ara
+{
+int sal=200000;
+}
+class aa extends ara 
+{
+int ben=2000;
+}
+class atharv
+{
+public static void main(String args[])
+{
+aa a=new aa();
+      System.out.println(" Salary :" +a.sal +"\nbenefit :"+a.ben);
+    }
+}
